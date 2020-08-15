36bd5eb3872f9c99bff5c09c2728c9f4ca53894b
code_bug <codebuggg@users.noreply.github.com>
2020-08-15 05:57:01 -0700

    adding url from cli

--- cli/apiToCsv.js
+++ cli/apiToCsv.js
@@ -1,11 +1,13 @@
 const getApiResponse = require('../src/getApiResponse');
 const convertToCsv = require('../src/convertToCsv');
+const {argv} = require('yargs');
 
 (async  () => {
-    const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
+    // console.log(process.argv[2])
+    // const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
 
-    const apiData = await getApiResponse(API_ENDPOINT);
+    const apiData = await getApiResponse(argv.url);
 
-    await convertToCsv(apiData);
+    await convertToCsv(apiData, "sample-ouput");
  
 })();
--- src/convertToCsv.js
+++ src/convertToCsv.js
@@ -2,8 +2,8 @@ const fastcsv = require('fast-csv');
 const fs = require('fs');
 
 
-module.exports = async function convertToCsv(data){
-  const ws = fs.createWriteStream("out.csv");
+module.exports = async function convertToCsv(data,filename){
+  const ws = fs.createWriteStream(`${filename}.csv`);
   fastcsv
     .write(data, { headers: true })
     .pipe(ws);
